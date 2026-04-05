7266ccc96f7913568efb184303d46634226c985b
Renan Mendes <renanmendes@discente.ufg.br>
2026-04-04 21:42:55 -0300

    Update ex1.go

--- LISTA.02 /ex1.go
+++ LISTA.02 /ex1.go
@@ -1,1 +1,15 @@
+package main
+import f "fmt" 
+var num int
 
+func main(){
+	f.Print("Digite um número inteiro qualquer: ")
+	f.Scan(&num)
+	if num%2 == 0 {
+		f.Printf("O número %d é PAR.\n",num)
+
+	}else{
+		f.Printf("O número %d é ÍMPAR.\n",num)
+	}
+		
+} 
